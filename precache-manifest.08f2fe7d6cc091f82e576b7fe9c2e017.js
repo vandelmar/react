3d902fbc5a7218fc340339279613e11978d4c56b
irfan.izi <irfan.izi.2020@gmail.com>
2020-12-28 16:38:35 +0700

    menyalin isi folder build

---
+++ precache-manifest.08f2fe7d6cc091f82e576b7fe9c2e017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cbbb67c6dcab247eb8e",
+    "url": "/static/css/main.abdfbbde.chunk.css"
+  },
+  {
+    "revision": "2cbbb67c6dcab247eb8e",
+    "url": "/static/js/main.2cbbb67c.chunk.js"
+  },
+  {
+    "revision": "7dc6bfac0cd7de58f5b9",
+    "url": "/static/js/1.7dc6bfac.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b2f3a6dd160196267d9305745e0e8e51",
+    "url": "/index.html"
+  }
+];
